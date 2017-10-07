03c5ed4a3b806a40860665580af6a50f9f8e41d7
olialka <olialka@corevalue.net>
2017-10-08 01:16:02 +0300

    Change client to webpack

---
+++ src/ngPlanner/client/app/planner-app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "planner-app",
+    templateUrl: "./planner-app.component.html",
+})
+export class PlannerAppComponent {
+
+}
---
+++ src/ngPlanner/client/common/common.module.ts
@@ -0,0 +1,11 @@
+import { CommonModule as AngularCommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+
+@NgModule({
+    declarations: [],
+    imports: [ CommonModule ],
+    exports: [],
+    providers: [],
+})
+export class CommonModule {
+}
